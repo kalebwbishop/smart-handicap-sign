25db002e3f849b8b558c703c3735aa639f291f6c
Kaleb Bishop <kalebwbishop@gmail.com>
2024-09-25 21:52:40 -0400

    Update model_classify.py

--- model_classify.py
+++ model_classify.py
@@ -1,9 +1,6 @@
 import math
 import json
 
-# REMOVE THIS LINE
-import numpy as np
-
 with open('./model_weights_biases.json', 'r') as f:
     model = json.load(f)
 
@@ -43,21 +40,6 @@ def classify(input_data):
     # Classify as True (1) if probability > 0.5, else False (0)
     return probability > 0.5
 
-# Example usage
-
-x = np.linspace(0, 256 * (1 / 25), 256)
-y = np.sin(2 * math.pi * x / 1.1)
-noise = np.random.normal(0, 0.5, y.shape)
-y += noise
-
-import matplotlib.pyplot as plt
-
-# Plotting y
-plt.plot(x, y)
-plt.title('Generated Data with Noise')
-plt.xlabel('x')
-plt.ylabel('y')
-# plt.show()
 
 input_data = y  # Replace with your 256-element input array
 classification = classify(input_data)
