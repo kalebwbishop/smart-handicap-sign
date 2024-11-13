19d3d1afa397960d0adfcbe51375fd61e0278745
Kaleb Bishop <Kalebwbishop@gmail.com>
2024-11-12 21:14:03 -0500

    Added notifications to flutter

---
+++ app/smart_handicap_sign_app/web/firebase-messaging-sw.js
@@ -0,0 +1,18 @@
+importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js");
+
+// todo Copy/paste firebaseConfig from Firebase Console
+const firebaseConfig = {
+ apiKey: "...",
+ authDomain: "...",
+ databaseURL: "...",
+ projectId: "...",
+ storageBucket: "...",
+ messagingSenderId: "...",
+ appId: "...",
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+// todo Set up background message handler
