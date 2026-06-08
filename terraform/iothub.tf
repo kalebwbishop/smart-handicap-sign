076e98187d05f6198b2d3e1b3dc097e8998b4009
Kaleb Bishop <Kalebwbishop@gmail.com>
2026-06-08 08:28:35 -0400

    feat: add workflow for cleaning up old workflow runs and stale caches

--- terraform/iothub.tf
+++ terraform/iothub.tf
@@ -1,13 +1,36 @@
 resource "azurerm_iothub" "this" {
-  name                = "hazardhero-iothub"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-
+  endpoint                     = []
+  enrichment                   = []
+  event_hub_partition_count    = 2
+  event_hub_retention_in_days  = 1
+  local_authentication_enabled = true
+  location                     = azurerm_resource_group.this.location
+  min_tls_version              = "1.2"
+  name                         = "hazardhero-iothub"
+  resource_group_name          = azurerm_resource_group.this.name
+  route                        = []
+  tags                         = {}
+  cloud_to_device {
+    default_ttl        = "PT1H"
+    max_delivery_count = 10
+    feedback {
+      lock_duration      = "PT1M"
+      max_delivery_count = 10
+      time_to_live       = "PT1H"
+    }
+  }
+  fallback_route {
+    condition      = "true"
+    enabled        = true
+    endpoint_names = ["events"]
+    source         = "DeviceMessages"
+  }
   sku {
-    name     = "F1"
     capacity = 1
+    name     = "F1"
   }
 }
+
 
 resource "azurerm_iothub_shared_access_policy" "service" {
   name                = "service"
